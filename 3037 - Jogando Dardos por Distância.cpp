1ca6b820f57bff3e0360228f92165a4b6cf71635
Willian Souza <76187694+BowserTx@users.noreply.github.com>
2021-03-28 21:32:46 -0400

    Uri code

---
+++ 3037 - Jogando Dardos por Distância.cpp
@@ -0,0 +1,46 @@
+#include iostream
+
+
+int main()
+{
+	int x, d, joao=0, maria=0, cases;
+
+	stdcin  cases;
+
+	for (; cases  0; cases--)
+
+	{
+		stdcin  x  d;
+
+		joao = (x  d) + joao;
+
+		stdcin  x  d;
+
+		joao = (x  d) + joao;
+
+		stdcin  x  d;
+
+		joao = (x  d) + joao;
+
+		stdcin  x  d;
+
+		maria = (x  d) + maria;
+
+		stdcin  x  d;
+
+		maria = (x  d) + maria;
+
+		stdcin  x  d;
+
+		maria = (x  d) + maria;
+
+		if (joao  maria)
+			stdcout  MARIA  n;
+
+		else
+			stdcout  JOAO  n;
+
+		joao = 0;
+		maria = 0;
+	}
+}
